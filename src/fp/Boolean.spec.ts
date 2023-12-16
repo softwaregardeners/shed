9c205cacb3e8a1c7b6a1d063a0120465f9779577
Brice Carpentier <brice@carpentier.club>
2023-12-16 12:40:12 +0100

    Improve tests organisation

--- src/fp/Boolean.spec.ts
+++ src/fp/Boolean.spec.ts
@@ -2,29 +2,33 @@ import { describe, expect, it } from "vitest"
 import * as B from "./Boolean.js"
 import { pipe } from "./Function.js"
 
-describe("isTrue", () => {
-    it.each([
-        [true, true],
-        [false, false],
-    ] as const)("[%s] returns %s", (input, output) => {
-        pipe(input, B.isTrue, expect).to.equal(output)
+describe("Predicates", () => {
+    describe("isTrue", () => {
+        it.each([
+            [true, true],
+            [false, false],
+        ] as const)("[%s] returns %s", (input, output) => {
+            pipe(input, B.isTrue, expect).to.equal(output)
+        })
+    })
+
+    describe("isFalse", () => {
+        it.each([
+            [true, false],
+            [false, true],
+        ] as const)("[%s] returns %s", (input, output) => {
+            pipe(input, B.isFalse, expect).to.equal(output)
+        })
     })
 })
 
-describe("isFalse", () => {
-    it.each([
-        [true, false],
-        [false, true],
-    ] as const)("[%s] returns %s", (input, output) => {
-        pipe(input, B.isFalse, expect).to.equal(output)
-    })
-})
-
-describe("not", () => {
-    it.each([
-        [true, false],
-        [false, true],
-    ] as const)("[%s] returns %s", (input, output) => {
-        pipe(input, B.not, expect).to.equal(output)
+describe("Combinators", () => {
+    describe("not", () => {
+        it.each([
+            [true, false],
+            [false, true],
+        ] as const)("[%s] returns %s", (input, output) => {
+            pipe(input, B.not, expect).to.equal(output)
+        })
     })
 })
