26191c276a5a6cab0109441eeb78c5a036803b3c
Brice Carpentier <brice@carpentier.club>
2023-12-16 18:51:30 +0100

    Improve Result.ts

--- src/fp/Result.spec.ts
+++ src/fp/Result.spec.ts
@@ -1,4 +1,5 @@
-import { assert, describe, it } from "vitest"
+import { assert, describe, expect, it } from "vitest"
+import { pipe } from "./Function.js"
 import * as Result from "./Result.js"
 
 describe("Constructors + Predicates", () => {
@@ -17,6 +18,27 @@ describe("Constructors + Predicates", () => {
             assert(!Result.isSuccess(actual), "IS a Success")
             assert(Result.isFailure(actual), "is NOT a Failure")
             assert(actual.cause === 42, "value NOT wrapped as expected")
+        })
+    })
+})
+
+describe("Destructors", () => {
+    describe("extract", () => {
+        it.each([
+            ["[success] returns the value", Result.success(42), 42],
+            ["[failure] returns the cause", Result.failure(42), 42],
+        ] as const)("%s", (_, input, expected) => {
+            pipe(input, Result.extract, expect).to.equal(expected)
+        })
+    })
+})
+
+describe("Applicative", () => {
+    describe("of", () => {
+        it("returns a success", () => {
+            const v = Result.of(42)
+            assert(Result.isSuccess(v), "is NOT a Success")
+            assert(!Result.isFailure(v), "IS a Failure")
         })
     })
 })
--- src/fp/Result.ts
+++ src/fp/Result.ts
@@ -1,12 +1,8 @@
+import * as Maybe from "./Maybe.js"
+
 export type t<E, A> = Success<A> | Failure<E>
 export type Success<A> = { _tag: "Success"; value: A }
 export type Failure<E> = { _tag: "Failure"; cause: E }
-
-/**
- * Constructors
- */
-export const success = <A>(v: A): Success<A> => ({ _tag: "Success", value: v })
-export const failure = <E>(e: E): Failure<E> => ({ _tag: "Failure", cause: e })
 
 /*/
  * Predicatess
@@ -15,3 +11,55 @@ export const isSuccess = <E, A>(v: t<E, A>): v is Success<A> =>
     v._tag === "Success"
 export const isFailure = <E, A>(v: t<E, A>): v is Failure<E> =>
     v._tag === "Failure"
+
+/**
+ * Constructors
+ */
+export const success = <A>(v: A): t<never, A> => ({ _tag: "Success", value: v })
+export const failure = <E>(e: E): t<E, never> => ({ _tag: "Failure", cause: e })
+
+export const fromMaybe =
+    <E>(onNothing: () => E) =>
+    <A>(v: Maybe.t<A>) =>
+        Maybe.isJust(v) ? success(v.value) : failure(onNothing())
+
+/**
+ * Destructors
+ */
+export function extract<A>(v: Success<A>): A
+export function extract<E>(v: Failure<E>): E
+export function extract<E, A>(v: t<E, A>): A | E
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function extract(v: t<any, any>): any {
+    return isSuccess(v) ? v.value : v.cause
+}
+/**
+ * Applicative
+ */
+export const of: <A>(v: A) => t<never, A> = success
+
+/**
+ * Apply
+ */
+export const ap =
+    <E, A>(v: t<E, A>) =>
+    <B>(fn: (v: A) => B) =>
+        map(fn)(v)
+
+/**
+ * Functor
+ */
+export const map =
+    <A, B>(fn: (a: A) => B) =>
+    <E>(v: t<E, A>): t<E, B> =>
+        isSuccess(v) ? success(fn(v.value)) : failure(v.cause)
+/**
+ * Chain
+ */
+export const chain =
+    <F, A, B>(fn: (v: A) => t<F, B>) =>
+    <E>(v: t<E, A>): t<E | F, B> =>
+        flatten(map(fn)(v))
+
+export const flatten = <E, F, A>(v: t<F, t<E, A>>): t<E | F, A> =>
+    isSuccess(v) ? v.value : failure(v.cause)
